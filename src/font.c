e9978ac94e234efa6a46d7ccd6c7f0b7ea6a7150
John Colburn <they.called.him.crazy@gmail.com>
2013-11-18 12:31:36 -0600

    Font now creates and stores an array of surfaces instead of one surface and many rects. resolves #2

--- src/font.c
+++ src/font.c
@@ -6,74 +6,77 @@ font_t *font_create(const char *image_fn){
     font_t *font = malloc(sizeof(font_t));
     memset(font, 0, sizeof(font_t));
 
-    font->src = load_image(image_fn);
+    SDL_Surface *font_img = load_image(image_fn);
     
-    uint32_t *pixels = font->src->pixels;
+    uint32_t *pixels = font_img->pixels;
 
     uint32_t this_mark = 0;
     uint32_t prev_mark = 0;
     uint32_t mark_color = pixels[0];
 
     uint32_t glyph_index = 0;
+    SDL_Rect glyph_rect;
+    SDL_Surface *glyph_surface;
+
     uint32_t glyph_count = wcslen(glyph_order);
-    
-    /* for(int i=0; i<MAX_GLYPHS; i++){
-        font->glyphs[i].x = 0;
-        font->glyphs[i].y = 0;
-        font->glyphs[i].w = 0;
-        font->glyphs[i].h = 0;
-    } */
     
     while(glyph_index < glyph_count){
         while(pixels[this_mark] == mark_color){
-            if(this_mark > font->src->w){ break; }
+            if(this_mark > font_img->w){ break; }
             this_mark += 1;
         }
         
         prev_mark = this_mark;
         
         while(pixels[this_mark] != mark_color){
-            if(this_mark > font->src->w){ break; }
+            if(this_mark > font_img->w){ break; }
             this_mark += 1;
         }
         
-        if(this_mark > font->src->w){
+        if(this_mark > font_img->w){
             fprintf(stderr, "Warning: font_init: Font source shorter than glyph list.\n");
             break;
         }
         
-        font->glyphs[glyph_order[glyph_index]].x = prev_mark;
-        font->glyphs[glyph_order[glyph_index]].w = this_mark - prev_mark;
-        font->glyphs[glyph_order[glyph_index]].y = 0;
-        font->glyphs[glyph_order[glyph_index]].h = font->src->h;
+        glyph_rect.x = prev_mark;
+        glyph_rect.w = this_mark - prev_mark;
+        glyph_rect.y = 0;
+        glyph_rect.h = font_img->h;
+
+        glyph_surface = create_surface(glyph_rect.w, glyph_rect.h);
+        SDL_BlitSurface(font_img, &glyph_rect, glyph_surface, NULL);
+        
+        font->glyphs[glyph_order[glyph_index]] = glyph_surface;
+
         glyph_index += 1;
     }
+    
+    SDL_FreeSurface(font_img);
     
     return font;
 }
 
 void font_delete(font_t *font){
-    SDL_FreeSurface(font->src);
+    for(int i=0; i<MAX_GLYPHS; i++){
+        if(font->glyphs[i] != NULL){
+            SDL_FreeSurface(font->glyphs[i]);
+        }
+    }
+    
     free(font);
 }
 
 void font_draw_all_glyphs(font_t *font, int x, int y, SDL_Surface *target){
-    SDL_Rect target_rect;
-    target_rect.x = x;
-    target_rect.y = y;
-    for(int i=0; i<MAX_GLYPHS; i++){
-        SDL_BlitSurface(font->src, &(font->glyphs[i]), target, &target_rect);
-        target_rect.x += font->glyphs[i].w;
-    }
+    font_draw_string(font, glyph_order, x, y, target);
 }
 
-void font_draw_string(font_t *font, wchar_t *string, int32_t x, int32_t y, SDL_Surface *target){
+void font_draw_string(font_t *font, const wchar_t *string, int32_t x, int32_t y, SDL_Surface *target){
     SDL_Rect target_rect;
     target_rect.x = x;
     target_rect.y = y;
     for(int i=0; i<wcslen(string); i++){
-        SDL_BlitSurface(font->src, &(font->glyphs[string[i]]), target, &target_rect);
-        target_rect.x += font->glyphs[string[i]].w;
+        SDL_BlitSurface(font->glyphs[string[i]], NULL, target, &target_rect);
+        target_rect.x += font->glyphs[string[i]]->w;
     }
 }
 
--- src/font.h
+++ src/font.h
@@ -12,14 +12,13 @@
 #define MAX_GLYPHS 256
 typedef struct font_t font_t;
 struct font_t{
-    SDL_Surface *src;
-    SDL_Rect glyphs[MAX_GLYPHS];
+    SDL_Surface *glyphs[MAX_GLYPHS];
 };
 
 font_t *font_create(const char *image_fn);
 void font_delete(font_t *font);
 
 void font_draw_all_glyphs(font_t *font, int x, int y, SDL_Surface *target);
-void font_draw_string(font_t *font, wchar_t *string, int32_t x, int32_t y, SDL_Surface *target);
+void font_draw_string(font_t *font, const wchar_t *string, int32_t x, int32_t y, SDL_Surface *target);
 
 #endif
