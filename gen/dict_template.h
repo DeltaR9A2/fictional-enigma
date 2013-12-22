6d8285399c7f9445cde6679a2581066c6059d61a
John Colburn <they.called.him.crazy@gmail.com>
2013-12-22 09:52:08 -0600

    Fixing circular include issues.

--- gen/dict_template.h
+++ gen/dict_template.h
@@ -1,17 +1,18 @@
 #ifndef TYPE_dict_h
 #define TYPE_dict_h
 
+typedef struct TYPE_node_t TYPE_node_t;
+typedef struct TYPE_dict_t TYPE_dict_t;
+
 #include <wchar.h>
 #include "TYPE.h"
 
-typedef struct TYPE_node_t TYPE_node_t;
 struct TYPE_node_t{
     TYPE_t *data;
     TYPE_node_t *next;
     wchar_t name[32];
 };
 
-typedef struct TYPE_dict_t TYPE_dict_t;
 struct TYPE_dict_t{
     TYPE_node_t *head;
 };
--- gen/list_template.h
+++ gen/list_template.h
@@ -1,18 +1,19 @@
 #ifndef TYPE_list_h
 #define TYPE_list_h
 
+typedef struct TYPE_node_t TYPE_node_t;
+typedef struct TYPE_list_t TYPE_list_t;
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <wchar.h>
 #include "TYPE.h"
 
-typedef struct TYPE_node_t TYPE_node_t;
 struct TYPE_node_t{
     TYPE_t *data;
     TYPE_node_t *next;
 };
 
-typedef struct TYPE_list_t TYPE_list_t;
 struct TYPE_list_t{
     TYPE_node_t *head;
 };
