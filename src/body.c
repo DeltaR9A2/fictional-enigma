0e2badef1e12288401b9c300e09e7e13a53b88ac
John Colburn <they.called.him.crazy@gmail.com>
2014-11-11 14:57:41 -0600

    Removing annoying tabs.

--- src/body.c
+++ src/body.c
@@ -2,13 +2,13 @@
 
 #include "body.h"
 
-const uint32_t BLOCKED_L =	  0x00000001;
-const uint32_t BLOCKED_R =	  0x00000002;
-const uint32_t BLOCKED_U =	  0x00000004;
-const uint32_t BLOCKED_D =	  0x00000008;
+const uint32_t BLOCKED_L = 0x00000001;
+const uint32_t BLOCKED_R = 0x00000002;
+const uint32_t BLOCKED_U = 0x00000004;
+const uint32_t BLOCKED_D = 0x00000008;
 const uint32_t BLOCKED_MASK = 0x0000000F;
 
-const uint32_t PLAT_DROP =	  0x00000010;
+const uint32_t PLAT_DROP = 0x00000010;
 
 body_t *body_create(void){
 	body_t *body = malloc(sizeof(body_t));
